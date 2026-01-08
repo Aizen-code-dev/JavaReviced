c27aa1dbfa992ac790014b51db8223fa8bda49e0
Aizen-code-dev <satishbhitale4321@gmail.com>
2026-01-08 14:36:05 +0530

    Its HelloWorld

---
+++ src/main/java/org/java/reviced/loop/ControlStatement.java
@@ -0,0 +1,25 @@
+package org.java.reviced.loop;
+
+public class ControlStatement {
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 5; i++) {
+
+
+
+            //break statement
+            if(i==4){
+                break;
+            }
+
+            //continue statement
+            if (i==2){
+                continue;
+            }
+
+            System.out.println(i);
+
+
+        }
+    }
+}
---
+++ src/main/java/org/java/reviced/loop/Forloop.java
@@ -0,0 +1,24 @@
+package org.java.reviced.loop;
+
+public class Forloop {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5 ; i++) {
+
+            System.out.print("*");
+
+
+        }
+
+
+        for (int i = 0; i < 5 ; i++) {
+
+
+            System.out.println("*");
+
+
+
+        }
+
+    }
+}
