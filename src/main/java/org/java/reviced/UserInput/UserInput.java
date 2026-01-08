6a07a2ae29958cc4c1140bf900a3193c57c60f1d
Aizen-code-dev <satishbhitale4321@gmail.com>
2026-01-08 19:41:16 +0530

    while loop

---
+++ src/main/java/org/java/reviced/UserInput/UserInput.java
@@ -0,0 +1,33 @@
+package org.java.reviced.UserInput;
+
+import java.util.*;
+
+public class UserInput {
+    public static void main(String[] args) {
+
+        //take input
+        Scanner sc =new Scanner(System.in);
+
+        System.out.print("Enter Number:");
+        int number = sc.nextInt();
+
+
+        sc.nextLine();
+
+
+        System.out.println("Enter Name:");
+        String name= sc.nextLine();
+
+        System.out.println("Enter age:");
+        float age= sc.nextFloat();
+
+        System.out.println(number);
+        System.out.println(name);
+        System.out.println(age);
+
+
+        sc.close();
+
+
+    }
+}
